86074e27b44654e3706b30e01b078b9d74420037
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-01-20 12:33:48 +0000

    seeding example data

--- model/tasks.js
+++ model/tasks.js
@@ -1,13 +1,20 @@
 const db = require('../database/db');
 
-const insert_task = db.prepare('INSERT INTO tasks (content) VALUES (?)');
+const insert_task = db.prepare(`
+    INSERT INTO tasks (content) 
+    VALUES (?)
+    RETURNING id, content, created_at
+`);
 
 function createTask(content) {
-    insert_task.run(content);
+    return insert_task.get(content);
 }
 
-createTask('This is a test task');
+createTask('This is a test task!');
 const tasks = db.prepare('SELECT * FROM tasks').all();
 console.log(tasks);
+
+const result = createTask('Buy ergo mouse');
+console.log(result);
 
 module.exports = { createTask };
