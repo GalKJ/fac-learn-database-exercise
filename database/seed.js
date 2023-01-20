98508c16008d0722650166ccb04d9f80b6db4335
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-01-20 13:26:52 +0000

    Named SQL parameters

--- database/seed.js
+++ database/seed.js
@@ -8,13 +8,3 @@ const seed = readFileSync(seedPath, 'utf-8');
 db.exec(seed);
 
 console.log('DB seeded successfully!');
-
-// const { readFileSync } = require('node:fs');
-// const { join } = require('node:path');
-// const db = require('./db.js');
-
-// const seedPath = join('database', 'seed.sql');
-// const seed = readFileSync(seedPath, 'utf-8');
-// db.exec(seed);
-
-// console.log('DB seeded with example data');
--- database/seed.sql
+++ database/seed.sql
@@ -1,22 +1,12 @@
--- BEGIN;
-
--- INSERT INTO tasks VALUES 
---     (4, 'Create my first todo', '2023-01-20 12:00:00'),
---     (5, 'Buy oat milk', '2023-01-20 12:01:00'),
---     (6, 'Go running and code', '2023-01-20 12:02:00')
--- ON CONFILCT(id) DO NOTHING;
-
--- COMMIT;
-
 BEGIN;
 
 INSERT INTO tasks VALUES
-  (1, 'Create my first todo', '2022-09-16 01:01:01'),
-  (2, 'Buy milk', '2022-09-16 11:10:07'),
-  (3, 'Become a 10x developer', '2022-09-16 23:59:59'),
-  (4, 'Create my first todo', '2023-01-20 12:00:00'),
-  (5, 'Buy oat milk', '2023-01-20 12:01:00'),
-  (6, 'Go running and code', '2023-01-20 12:02:00')
+  (1, 'Create my first todo', '2022-09-16 01:01:01', 1),
+  (2, 'Buy milk', '2022-09-16 11:10:07', 1),
+  (3, 'Become a 10x developer', '2022-09-16 23:59:59', 0),
+  (4, 'Create my first todo', '2023-01-20 12:00:00', 0),
+  (5, 'Buy oat milk', '2023-01-20 12:01:00', 1),
+  (6, 'Go running and code', '2023-01-20 12:02:00', 1)
 ON CONFLICT(id) DO NOTHING;
 
 COMMIT;
